975f758918d52c8b71fc9721302c794f5682189b
Jonathan Hansen <j.hansen.oh@gmail.com>
2019-11-07 20:22:30 -0500

    Update Checkers hpp. Edited some concepts int hpp, and began coding in cpp. Started Class board. began constructor function.

---
+++ checkers.cpp
@@ -0,0 +1,15 @@
+
+
+#include "checkers.hpp"
+/////////////////////////////////////////////////
+// Board Class
+/////////////////////////////////////////////////
+void board::board(int size = 8) {
+	boardSize = size;
+	bool isEven = true;
+	for (int col = 0; col < boardSize; col++) {
+		for (int row = 0; row < boardSize; col++) {
+			
+		}
+	}
+}
--- checkers.hpp
+++ checkers.hpp
@@ -2,15 +2,58 @@
 #ifndef CHECKERS_HPP_
 #define CHECKERS_HPP_
 
-class board {
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
+class board {
+public:
+	board(int size);
+	void printBoard(); // prints board
+	void movePiece(); // moves a piece
+
+private:
+	piece* selectPiece(); // used in movePiece(), selects piece to move
+	square* selectMoveTo(); // used in movePiece(), selects target square
+	bool check(); // checks if square is a valid move (diagonal, not occupied)
+	bool checkJump(); // checks if a jump is being made
+	void takePiece(); // if checkJump() remove piece from board
+
+	std::vector< std::vector<square*> > gameboard;
+	char turn;
+	int boardSize; // Not including boarders
+	piece* selectedPi;
+	square* selectedSq
+	std::vector<square>;
+	std::vector<piece>;
 };
 
 class square {
+public:
+	square(char color, int x, int y);
+	void occupation(bool status, char team);
+	piece occupier();
 
+private:
+	char color;
+	bool occupied;
+	piece* onSquare
+	int x, y;
 };
 
 class piece {
+public:
+	piece(char team, int x, int y)
+	void kinged();
+	void move(int x, int y);
+
+private:
+	square* onSquare;
+	int x, y;
+	char team; // X or O
+	bool kinged; // True changes char to K or Q
 
 };
 
@@ -29,9 +72,9 @@ class piece {
 // +-+-+-+-+-+-+-+-+
 // | | | | | | | | |
 // +-+-+-+-+-+-+-+-+
-// | |#| |#| |#| |#|
+// |X|#|X|#|X|#|X|#|
 // +-+-+-+-+-+-+-+-+
-// |#| |#| |#| |#| |
+// |#|X|#|X|#|X|#|X|
 // +-+-+-+-+-+-+-+-+
 
 #endif
